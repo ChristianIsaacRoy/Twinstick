8a65c11b657d5c7e88d4bd65a8dbfd13ab7d6361
Christian <christian.50.roy@gmail.com>
2018-04-15 13:35:09 -0600

    Edit Item Table to be more appealing.

--- Assets/Editor/ItemTable.cs
+++ Assets/Editor/ItemTable.cs
@@ -20,6 +20,7 @@ public class ItemTable : EditorWindow
         items = FindAssetsByType<ItemBlueprint>();
 
         reorderableList = new ReorderableList(items, typeof(ItemBlueprint), true, true, true, true);
+        reorderableList.elementHeight *= 3;
 
         // This could be used aswell, but I only advise this your class inherrits from UnityEngine.Object or has a CustomPropertyDrawer
         // Since you'll find your item using: serializedObject.FindProperty("list").GetArrayElementAtIndex(index).objectReferenceValue
@@ -42,8 +43,7 @@ public class ItemTable : EditorWindow
 
     private void DrawHeader(Rect rect)
     {
-        GUI.Label(rect, "Our fancy reorderable list");
-        GUI.Label(rect, "Description");
+        GUI.Label(rect, "Items List");
     }
 
     private void DrawElement(Rect rect, int index, bool active, bool focused)
@@ -52,15 +52,28 @@ public class ItemTable : EditorWindow
 
         EditorGUI.BeginChangeCheck();
 
-        Rect col1 = new Rect(rect.x + 0 * rect.width / 6, rect.y, rect.width / 6, rect.height);
-        Rect col2 = new Rect(rect.x + 1 * rect.width / 6, rect.y, rect.width - 3 * rect.width / 6, rect.height);
-        Rect col3 = new Rect(rect.x + 2 * rect.width / 6, rect.y, rect.width - rect.width / 6, rect.height);
-        Rect col4 = new Rect(rect.x + 3 * rect.width / 6, rect.y, rect.width - rect.width / 6, rect.height);
+        int labelWidth = 45;
 
-        item.name = EditorGUI.TextField(col1, item.name);
-        item.value = EditorGUI.IntField(col2, item.value);
+        Rect labelBox = new Rect(rect.x, rect.y, rect.width / 6, rect.height / 3);
+        Rect valueBox = new Rect(rect.x + labelWidth, rect.y, rect.width / 6 - labelWidth, rect.height / 3);
+        EditorGUI.LabelField(labelBox, "Name:");
+        item.name = EditorGUI.TextField(valueBox, item.name);
+
+        labelBox = new Rect(rect.x, rect.y + rect.height / 3, rect.width / 6, rect.height / 3);
+        valueBox = new Rect(rect.x + labelWidth, rect.y + rect.height / 3, rect.width / 6 - labelWidth, rect.height / 3);
+        EditorGUI.LabelField(labelBox, "Value:");
+        item.value = EditorGUI.IntField(valueBox, item.value);
+        
+        valueBox = new Rect(rect.x + labelWidth, rect.y + 2 * rect.height / 3, rect.width / 6 - labelWidth, rect.height / 3);
+        item.rarity = (Rarity)EditorGUI.EnumPopup(valueBox, item.rarity);
+
+        valueBox = new Rect(rect.x + 0 * rect.width / 6, rect.y + rect.height/3, rect.width / 6, rect.height/3);
+        Rect col3 = new Rect(rect.x + 2 * rect.width / 6, rect.y, rect.width / 6, rect.height/3);
+        Rect col4 = new Rect(rect.x + 3 * rect.width / 6, rect.y, rect.width / 6, rect.height);
+        
         item.rarity = (Rarity)EditorGUI.EnumPopup(col3, item.rarity);
-        item.description = EditorGUI.TextField(col4, item.description);
+        item.icon = (Sprite)EditorGUI.ObjectField(col4, item.icon, typeof(Sprite), false);
+        //item.description = EditorGUI.TextField(col4, item.description);
 
         if (EditorGUI.EndChangeCheck())
         {
